22dd8286a3c37f41be94bd103e584d92248684f8
steph-harris <steph.harris21@gmail.com>
2016-02-09 18:22:38 -0500

    added cookies that last for 1 minute, added dynamic route catch all error

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require("express");
-var app = express();
 var session = require("express-session");
+
+var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use("/scripts", express.static("public/scripts"));
 app.use("/css", express.static("public/css"));
 app.use("/images", express.static("public/images"));
 //session middleware
-app.use(session({
-
-}));
+app.use(
+  session({
+    secret: 'my secret',
+    cookie: {
+      maxAge:60000
+    },
+    saveUninitialized: true,
+    resave: false
+  }
+));
 
 app.get("/", function(req, res){
+  var sess = req.session;
   res.sendFile(process.cwd() + "/views/home.html");
 });
 app.get("/projects", function(req, res){
@@ -22,6 +31,9 @@ app.get("/blog", function(req, res){
 });
 app.get("/showcase", function(req, res){
   res.sendFile(process.cwd() + "/views/showcase.html");
+});
+app.get("/:anything_else", function(req, res){
+  res.send("That file doesn't exist. Did you mean to put / ?");
 });
 
 app.listen(PORT, function(){
