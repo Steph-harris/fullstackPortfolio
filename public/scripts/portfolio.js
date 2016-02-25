33d4021729a1967479c603ec489ffe8e16331bfc
steph-harris <steph.harris21@gmail.com>
2016-02-25 16:34:26 -0500

    fixed navbar not closing on selection, added js logic to keep nav from closing on dropdown selection

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-   // make navbar only affix from about me section
- $('#sidebar').affix({
-    offset: {
-    top: 715
+  //close navbar on selection
+  var navCol = $(".navbar-collapse");
+  var navLink = $(".navbar-nav a");
+
+  navLink.on("click", function(){
+    //logic to keep nav from closing on dropdown-toggle
+    if($(this).hasClass("dropdown-toggle")){
+      return
     }
+    navCol.collapse('hide');
   });
+
+   // make navbar only affix from about me section
+ // $('#sidebar').affix({
+ //    offset: {
+ //    top: 715
+ //    }
+ //  });
 
   var $body   = $(document.body);
   var homeHeight = $('#home').outerHeight(true);
