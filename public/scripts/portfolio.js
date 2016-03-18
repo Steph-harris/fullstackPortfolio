d9815db1f411d6104d5980430ac0dc0b83b57dac
steph-harris <steph.harris21@gmail.com>
2016-03-18 02:52:31 -0400

    rewrote projects as dynamically created objects, rewrote jquery size code to accomodate

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,32 +1,15 @@
 $(document).ready(function(){
-  //close navbar on selection
-  var navCol = $(".navbar-collapse");
-  var navLink = $(".navbar-nav a");
-  //.navbar-collapse a needs to close nav too
-
-  // navCol.on("blur", function(){
-  //   navCol.collapse('hide');
-  // }
-  navLink.on("click", function(){
-    //add logic to close nav if it's not in focus
-    //logic to keep nav from closing on dropdown-toggle
-    if($(this).hasClass("dropdown-toggle")){
-      return
-    }
-    navCol.collapse('hide');
+  $('.main-content-wrapper').stickyStack({
+    containerElement: '.main-content-wrapper',
+    stackingElement:  'section',
+    boxShadow:      '0 -3px 20px rgba(0, 0, 0, 0.25)'
   });
-
-$('.main-content-wrapper').stickyStack({
-  containerElement: '.main-content-wrapper',
-  stackingElement:  'section',
-  boxShadow:      '0 -3px 20px rgba(0, 0, 0, 0.25)'
-});
 
   var $body   = $(document.body);
   var homeHeight = $('#home').outerHeight(true);
 
   $body.scrollspy({
-    target: '#sidebar',
+    target: 'ul.sidebar-nav li',
   });
 
   // navbar JS
@@ -57,13 +40,5 @@ $('.main-content-wrapper').stickyStack({
         }
       }
     });
-  });
-
-  $( window ).resize(function() {
-    debugger;
-    var maxHeight = 0;
-    maxHeight = $("#bigPic").height();
-
-    $("div .show-im").height(maxHeight);
   });
 });
---
+++ public/scripts/projects.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+  //Create Project Objects
+  var projects = [
+    {
+      title: "Show Us a Pet",
+      pic:"/images/petstiny.png" ,
+      href:"https://showusapet.herokuapp.com"
+    }, {
+      title: "Rutgers Flyers",
+      pic: "/images/rufly.png",
+      href:"https://rufly2.herokuapp.com"
+    }, {
+      title: "Jackie Minieri Fashion",
+      pic: "/images/jmfashion.png",
+      href:"https://jackieminieri.herokuapp.com"
+    }, {
+      title: "Rock, Paper, Scissors",
+      pic: "/images/rpstiny.png",
+      href:"https://stephs-rps.herokuapp.com"
+    }, {
+      title: "Clicky Games",
+      pic: "/images/clickytiny.png",
+      href:"https://candsclicky.herokuapp.com"
+    }, {
+      title: "My First Portfolio",
+      pic: "/images/shweb.png",
+      href:"http://painter-mildred-27247.bitballoon.com/"
+    }
+  ]
+
+  for(var i=0; i<projects.length; i++){
+    var newProj = '<div class="col-xs-12 col-sm-6 col-md-4 show-im">'
+    newProj += '<a href="' + projects[i].href + '"target="_blank">'
+    newProj += '<img id="projIm' + [i+1] + '" class="img-responsive fitRow "'
+    newProj += 'src="' + projects[i].pic + '"></a></div>';
+
+    console.log(newProj);
+    $(".projectIms").append(newProj);
+  }
+
+  // changes all project images to height of biggest pic
+  $(window).resize(function() {
+    debugger;
+    var maxHeight = 0;
+    maxHeight = $("#projIm1").height();
+
+    $("div .show-im").height(maxHeight);
+    // $(".fitRow").height(maxHeight);
+  });
+
+  $(".show-im").on("mouseenter", function(){
+    console.log(this);
+  });
+});
+
+
