a6499ddfd0a13dc4c53a78572ea232dd8bfb8d78
steph-harris <steph.harris21@gmail.com>
2016-03-18 15:50:17 -0400

    added summaries and pics to madals

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -1,49 +1,22 @@
 $(document).ready(function(){
-  for(var i=0; i<projects.length; i++){
-    //build div with project image that toggles modal
-    var newProj = '<div class="col-xs-12 col-sm-6 col-md-4 show-im">'
-    newProj += '<a data-toggle="modal" data-target="#modal'+ [i+1] + '" target="_blank">'
-    newProj += '<img id="projIm' + [i+1] + '" class="img-responsive fitRow "'
-    newProj += 'src="' + projects[i].pic + '"></a></div>';
-
-    //build modal with this objects' info
-    var newModal = '<div class="modal fade" id="modal'+ [i+1] + '"'
-    newModal += 'tabindex="-1" role="dialog"'
-    newModal += 'aria-labelledby="mySmallModalLabel">'
-    newModal += '<div class="modal-dialog" role="document">'
-    newModal += '<div class="modal-content"><div class="modal-header">'
-    newModal += '<button type="button" class="close" data-dismiss="modal"'
-    newModal += 'aria-label="Close"><span aria-hidden="true">&times;</span></button>'
-    newModal += '<h4 class="modal-title" id="modLabel' + [i+1] +'">'
-    newModal += projects[i].title + '</h4></div>'
-    newModal += '<div class="modal-body"></div>'
-    newModal += '<div class="modal-footer">'
-    newModal += '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
-    newModal += '<button type="button" class="btn btn-primary">Save changes</button>'
-    newModal += '</div></div></div></div>'
-
-    console.log(newModal);
-    console.log(newProj);
-    $(".projectIms").append(newProj).append(newModal);
-  }
+  var newModal = '<div class="modal fade" id="modal'+ [i+1] + '"'
+  newModal += 'tabindex="-1" role="dialog"'
+  newModal += 'aria-labelledby="mySmallModalLabel">'
+  newModal += '<div class="modal-dialog" role="document">'
+  newModal += '<div class="modal-content"><div class="modal-header">'
+  newModal += '<button type="button" class="close" data-dismiss="modal"'
+  newModal += 'aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+  newModal += '<h2 class="modal-title" id="modLabel' + [i+1] +'">'
+  newModal += '<a href="' + projects[i].href + '" target="_blank">'
+  newModal += projects[i].title + '</a>'
+  newModal += '<a href="'+ projects[i].github + '" target="_blank">'
+  newModal += '<i class="fa fa-github"></i></a>'
+  newModal += '</h2></div>'
+  newModal += '<div class="modal-body">'
+  newModal += '<h3>Summary</h3>'
+  newModal += '<p>' + projects[i].summary + '</p></div>'
+  newModal += '<div class="modal-footer">'
+  newModal += '<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'
+  newModal += '</div></div></div></div>'
 });
 
-<!-- Button trigger modal -->
-<!-- Modal -->
-// <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  // <div class="modal-dialog" role="document">
-    // <div class="modal-content">
-    //   <div class="modal-header">
-        // <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      //   ">Modal title</h4>
-      // </div>
-      // <div class="modal-body">
-      //   ...
-      // </div>
-      // <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -2,31 +2,44 @@ $(document).ready(function(){
   //Create Project Objects
   var projects = [
     {
-      title: "Show Us a Pet",
+      title: "Show Us a Pet ",
       pic:"/images/petstiny.png" ,
-      href:"https://showusapet.herokuapp.com"
+      href:"https://showusapet.herokuapp.com",
+      github:"https://github.com/Steph-harris/ourPetProject",
+      summary:"This is a shelter pet app that displays pets up for adoption close to a users zip code with images (from Flickr and specific shelters), pet description, special needs, wikipedia article about that particular breed (if one is provided) as well as care information. Users can also leave a review of their pet via Firebase chat."
     }, {
-      title: "Rutgers Flyers",
+      title: "Rutgers Flyers ",
       pic: "/images/rufly.png",
-      href:"https://rufly2.herokuapp.com"
+      href:"https://rufly2.herokuapp.com",
+      github:"https://github.com/tammerg/rutgers_flyer",
+      summary:"Group Project app that lets users sign up, add restaurants, and leave reviews."
     }, {
-      title: "Jackie Minieri Fashion",
+      title: "Jackie Minieri Fashion ",
       pic: "/images/jmfashion.png",
-      href:"https://jackieminieri.herokuapp.com"
+      href:"https://jackieminieri.herokuapp.com",
+      github:"https://github.com/Steph-harris/fashionSite",
+      summary:"I built this site for a client who is starting her own fashion line based on marine life designs.  She wanted a portfolio site that will eventually display her products. I set up the site in an MVC layout so that later we can easily work with a database and use Handlebars for the views."
     }, {
-      title: "Rock, Paper, Scissors",
+      title: "Rock, Paper, Scissors ",
       pic: "/images/rpstiny.png",
-      href:"https://stephs-rps.herokuapp.com"
+      href:"https://stephs-rps.herokuapp.com",
+      github:"https://github.com/Steph-harris/RPSGame",
+      summary:"My version of the popular game featuring Justin Aguilar's CSS3 animations. Users can play any odd number of rounds from 3-9, and any round ending in a tie will be replayed so that there is always a winner. I used Firebase to build a realtime Leaderboard for scores."
     }, {
-      title: "Clicky Games",
+      title: "Clicky Games ",
       pic: "/images/clickytiny.png",
-      href:"https://candsclicky.herokuapp.com"
+      href:"https://candsclicky.herokuapp.com",
+      github:"https://github.com/Steph-harris/jsClickGame" ,
+      summary:"This was a 2 part assignment that took place a few months apart: first we were to create a clicky game using vanilla javascript that counted and displayed how many faces a user was able to click within 20 seconds (clicky game #2). After we learned jQuery, we were tasked with taking a friend's game and updating it. I added a visible point display, modals, picture background colors based on game state when on(clicked or unclicked), and made the css names more semantic.  I also added my stamp of approval to the bottom of the page which verifies that it passed JS linting."
     }, {
-      title: "My First Portfolio",
+      title: "My First Portfolio ",
       pic: "/images/shweb.png",
-      href:"http://painter-mildred-27247.bitballoon.com/"
+      href:"http://painter-mildred-27247.bitballoon.com/",
+      github:"https://github.com/Steph-harris/my_cf_page",
+      summary:"This is the portfolio that I built as my introduction to web development in 2015"
     }
   ]
+
  for(var i=0; i<projects.length; i++){
    //build div with project image that toggles modal
    var newProj = '<div class="col-xs-12 col-sm-6 col-md-4 show-im">'
@@ -34,47 +47,33 @@ $(document).ready(function(){
    newProj += '<img id="projIm' + [i+1] + '" class="img-responsive fitRow "'
    newProj += 'src="' + projects[i].pic + '"></a></div>';
 
-   //build modal with this objects' info
-   var newModal = '<div class="modal fade" id="modal'+ [i+1] + '"'
-   newModal += 'tabindex="-1" role="dialog"'
-   newModal += 'aria-labelledby="mySmallModalLabel">'
-   newModal += '<div class="modal-dialog" role="document">'
-   newModal += '<div class="modal-content"><div class="modal-header">'
-   newModal += '<button type="button" class="close" data-dismiss="modal"'
-   newModal += 'aria-label="Close"><span aria-hidden="true">&times;</span></button>'
-   newModal += '<h4 class="modal-title" id="modLabel' + [i+1] +'">'
-   newModal += projects[i].title + '</h4></div>'
-   newModal += '<div class="modal-body"></div>'
-   newModal += '<div class="modal-footer">'
-   newModal += '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
-   newModal += '<button type="button" class="btn btn-primary">Save changes</button>'
-   newModal += '</div></div></div></div>'
+    var newModal = '<div class="modal fade" id="modal'+ [i+1] + '"'
+    newModal += 'tabindex="-1" role="dialog"'
+    newModal += 'aria-labelledby="mySmallModalLabel">'
+    newModal += '<div class="modal-dialog" role="document">'
+    newModal += '<div class="modal-content"><div class="modal-header">'
+    newModal += '<button type="button" class="close" data-dismiss="modal"'
+    newModal += 'aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+    newModal += '<h2 class="modal-title" id="modLabel' + [i+1] +'">'
+    newModal += '<a href="' + projects[i].href + '" target="_blank">'
+    newModal += projects[i].title + '</a>'
+    newModal += '<a href="'+ projects[i].github + '" target="_blank">'
+    newModal += '<i class="fa fa-github"></i></a>'
+    newModal += '</h2></div>'
+    newModal += '<div class="modal-body">'
+    newModal += '<img class="img-responsive" src="'+ projects[i].pic +'">'
+    newModal += '<h3>Summary</h3>'
+    newModal += '<p>' + projects[i].summary + '</p></div>'
+    newModal += '<div class="modal-footer">'
+    newModal += '<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'
+    newModal += '</div></div></div></div>'
 
-   console.log(newModal);
-   console.log(newProj);
    $(".projectIms").append(newProj).append(newModal);
  }
-  //ORIGINAL
-  // for(var i=0; i<projects.length; i++){
-  //   //build div with project image that toggles modal
-  //   var newProj = '<div class="col-xs-12 col-sm-6 col-md-4 show-im">'
-  //   newProj += '<a data-toggle="modal" data-target="#modal'+ [i+1] + '" target="_blank">'
-  //   newProj += '<img id="projIm' + [i+1] + '" class="img-responsive fitRow "'
-  //   newProj += 'src="' + projects[i].pic + '"></a></div>';
 
-  //   //build modal with this objects' info
-  //   var newModal = '<div class="modal fade" id="modal'+ [i+1] + '"'
-  //   newModal += 'tabindex="-1" role="dialog"'
-  //   newModal += 'aria-labelledby="mySmallModalLabel">'
-  //   newModal += '<div class="modal-dialog modal-sm">'
-  //   newModal += '<div class="modal-content">'
-  //   newModal += projects[i].title
-  //   newModal += '</div></div></div>'
-
-  //   console.log(newModal);
-  //   console.log(newProj);
-  //   $(".projectIms").append(newProj).append(newModal);
-  // }
+ // <a href="https://candsclicky.herokuapp.com"
+ // target="_blank"><img class="img-responsive"
+ // src="/images/clicky.png"></a>
 
   // changes all project images to height of biggest pic...
   function picEqual() {
@@ -93,7 +92,7 @@ $(document).ready(function(){
   });
 
   $(".show-im").on("mouseenter", function(){
-      console.log(this);
+      // console.log(this);
   });
 });
 //
