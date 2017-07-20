0e5c53fe9959fb3e232485e928a35800c2291a17
steph-harris <steph.harris21@gmail.com>
2017-07-19 21:33:49 -0400

    fixing broken github link, adding dynamic year

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,49 +1,10 @@
 $(document).ready(function(){
-  //close navbar on selection
   var navCol = $(".navbar-collapse");
   var navLink = $(".navbar-nav a");
-  //.navbar-collapse a needs to close nav too
-
-  // navCol.on("blur", function(){
-  //   navCol.collapse('hide');
-  // }
-  navLink.on("click", function(){
-    //add logic to close nav if it's not in focus
-    //logic to keep nav from closing on dropdown-toggle
-    if($(this).hasClass("dropdown-toggle")){
-      return
-    }
-    navCol.collapse('hide');
-  });
-
-$('.main-content-wrapper').stickyStack({
-  containerElement: '.main-content-wrapper',
-  stackingElement:  'section',
-  boxShadow:      '0 -3px 20px rgba(0, 0, 0, 0.25)'
-});
-
+  var today = new Date();
   var $body   = $(document.body);
   var homeHeight = $('#home').outerHeight(true);
 
-  $body.scrollspy({
-    target: '#sidebar',
-  });
-
-  // navbar JS
-  <!-- Custom Theme JavaScript -->
-   // Closes the sidebar menu
-  $("#menu-close").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-  });
-
-   // Opens the sidebar menu
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-  });
-
-  // Scrolls to the selected menu item on the page
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
@@ -57,5 +18,36 @@ $('.main-content-wrapper').stickyStack({
         }
       }
     });
+  });
+
+  $("#extras").text("\u00A9 "+today.getFullYear()+" Stephanie Harris");
+
+  navLink.on("click", function(){
+    if($(this).hasClass("dropdown-toggle")){
+      return
+    }
+    navCol.collapse('hide');
+  });
+
+  $('.main-content-wrapper').stickyStack({
+    containerElement: '.main-content-wrapper',
+    stackingElement:  'section',
+    boxShadow:      '0 -3px 20px rgba(0, 0, 0, 0.25)'
+  });
+
+  $body.scrollspy({
+    target: '#sidebar',
+  });
+
+   // Closes the sidebar menu
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+   // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
   });
 });
